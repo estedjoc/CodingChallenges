17bd58bb3d07668a838a9d821ab77d374f65bd6b
Esteban Orellana <esteban.orellana434@gmail.com>
2022-05-11 19:59:49 -0400

    second coding challenge done on May 11 2022

---
+++ 8 Kyu/convert-number-to-reversed-array/main.js
@@ -0,0 +1,4 @@
+const reverseNumToArr = (num) => num.toString().split('').reverse().map(num => Number(num));
+
+//initially forgot to change the contents of the array back into numbers from being a set of strings
+
