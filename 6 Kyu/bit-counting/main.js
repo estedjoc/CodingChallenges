133a1e1d2edbed8cbef153442bf2b8015947126b
Esteban Orellana <esteban.orellana434@gmail.com>
2022-05-25 20:12:56 -0400

    one of these i am surprised I got first try May 25 2022

---
+++ 6 Kyu/bit-counting/main.js
@@ -0,0 +1,10 @@
+const countBits = (n) => {
+
+    onceEmptyArray = []
+
+    let numberString = n.toString()
+
+    for (let i = 0; i < numberString.length; i++){
+
+    }
+}
---
+++ 8 Kyu/coefficients-of-the-quadratic-equation/main.js
@@ -0,0 +1,11 @@
+const quadratic = (x1, x2) => {
+    let a = 1 * 1;
+    let b = 1 * -x1;
+    let c = 1 * -x2;
+    let d = -x1 * -x2;
+    let e = b + c;
+
+    return [a, e, d];
+}
+
+//I genuinely can't believe I had gotten this first try smh I am actually so upset right now
