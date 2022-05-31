4d28490e5c1f4f9fc3292894ca8899dfffdc481a
Esteban Orellana <esteban.orellana434@gmail.com>
2022-05-30 22:18:26 -0400

    got closer with some code on a 6Kyu but failed 5 of 100 tests done. will try again tomorrow or another day. this was done on May 30 2022

--- 6 Kyu/bit-counting/main.js
+++ 6 Kyu/bit-counting/main.js
@@ -1,10 +1,12 @@
 const countBits = (n) => {
+    let bitNumber = (n >>> 0).toString(2);
 
-    onceEmptyArray = []
-
-    let numberString = n.toString()
-
-    for (let i = 0; i < numberString.length; i++){
-
+    if (bitNumber.toString().length >= 10){
+        return bitNumber.toString().split('').map( element => Number(element) ).reduce( (previousVal, currentVal) => previousVal + currentVal, 1 )
+    }else{
+        return bitNumber.toString().split('').map( element => Number(element) ).reduce( (previousVal, currentVal) => previousVal + currentVal, 0 );
     }
 }
+
+//made some more progress here today 05/30/22
+
