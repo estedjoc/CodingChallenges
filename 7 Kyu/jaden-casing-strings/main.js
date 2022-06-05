046124630d5f814221797675ca5d37bc40557023
Esteban Orellana <esteban.orellana434@gmail.com>
2022-06-04 21:27:00 -0400

    comment added June 4 2022

--- 7 Kyu/jaden-casing-strings/main.js
+++ 7 Kyu/jaden-casing-strings/main.js
@@ -7,3 +7,5 @@ const toJadenCase = (str) => {
 
 //not completed, still really confuses me because of question prompt and strcuture, uses string.prototype.toJadenCase really weird IMO setup but oh well will try another
 
+
+//I need to find a way to see if this works without the weird syntax in the original problem.
