7f5fc4795330f819368ca0f5a0b62ae14d0737c6
Esteban Orellana <esteban.orellana434@gmail.com>
2022-10-13 08:23:35 -0400

    codewars done on sept 27th

---
+++ 8 Kyu/main.js
@@ -0,0 +1,33 @@
+// Task
+
+// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+// Mind the input validation.
+// Example
+
+// { 6, 2, 1, 8, 10 } => 16
+// { 1, 1, 11, 2, 3 } => 6
+
+// Input validation
+
+// If an empty value ( null, None, Nothing etc. ) is given instead of an array, or the given array is an empty list or a list with only 1 element, return 0.
+
+const sumArray = array => {
+    
+    if ((array === null) || (array === []) || (array === undefined) || (array.length === 1) || (array.length === 2)){
+      return 0
+    }else{
+      let sortedArray = array.sort((a, b) => a - b);
+      
+    
+      let sumOfMiddleIntegers = 0;
+  
+      for (let i = 1; i < sortedArray.length - 1; i++){
+        sumOfMiddleIntegers += sortedArray[i]
+      }
+    
+      return sumOfMiddleIntegers
+    } 
+}
